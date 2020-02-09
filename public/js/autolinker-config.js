0705646db3398432637d10f6e134184ac4dfa1e6
Elliott Cost <elliottcost@gmail.com>
2020-02-09 01:26:35 -0500

    auto linker

---
+++ public/js/autolinker-config.js
@@ -0,0 +1,33 @@
+var autolinker = new Autolinker( {
+    urls : {
+        schemeMatches : true,
+        wwwMatches    : true,
+        tldMatches    : true
+    },
+    email       : false,
+    phone       : false,
+    mention     : false,
+    hashtag     : false,
+
+    stripPrefix : true,
+    stripTrailingSlash : true,
+    newWindow   : false,
+
+    truncate : {
+        length   : 0,
+        location : 'end'
+    },
+
+    className : ''
+} );
+
+var description = document.getElementById('description');
+var sections = document.getElementsByTagName('section');
+
+description.innerHTML = Autolinker.link(description.innerHTML);
+
+console.log(sections);
+
+for (const section of sections) {
+    section.innerHTML = Autolinker.link(section.innerHTML);
+}
