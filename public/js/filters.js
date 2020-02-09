da826267b044e23b6852662e9af5e2d01950c796
rene ae <aehtyb@gmail.com>
2020-02-08 20:01:21 -0600

    crayon

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,7 +1,50 @@
 const filterButtons = document.querySelectorAll('.filter');
 const people = document.querySelectorAll('.person');
 
+// this could be maybe obfuscated with https://obfuscator.io if you'd like
+var egg = []
+var eggon = false
+var boxon = false
+function easter(e) {
+  try {
+    var index = Array.from(e.parentNode.children).indexOf(e)
+    egg.push(index)
+    egg = egg.slice(-6)
+    var eastereggs = egg.join("").toString()
+    if (eastereggs === "010201") {
+      if (eggon) {
+        return
+      }
+      eggon = true
+      document.querySelectorAll("nav a.box.small")[0].href = "#"
+      document.querySelectorAll("nav a.box.small")[0].style.backgroundColor="green"
+      document.querySelectorAll("nav a.box.small")[0].addEventListener("click", () => {
+        if (boxon) {
+          return
+        }
+        boxon = true
+        filterButtons.forEach((filter) => {
+          filter.style.cssText = "border: none"
+        })
+        people.forEach((person) => {
+          person.style.display = "inline-block"
+        })
+        var head = document.getElementsByTagName('head')[0]
+        var js = document.createElement("script")
+        head.appendChild(js)
+        js.onload = () => {
+          window.phase()
+        }
+        js.src = "/js/crayon.min.js"
+      })
+    }
+  }
+  catch(e) {
+  }
+}
+
 function filterShape(clickedElement) {
+    easter(clickedElement)
     filterButtons.forEach((filter) => {
         filter.style.cssText = "border: 1px solid transparent"
     })
